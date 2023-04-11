98f190865e4afb3de2e3e37095330129ff3d28f8
João Victor Vilela dos Santos <118611278+vilelas@users.noreply.github.com>
2023-04-10 22:44:45 -0300

    swagger
    adicionando a documentação da api com o swagger

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 
-const authRoute = require("./routes/auth");
-const usuariosRoute = require("./routes/usuario");
-const tarefaRoute = require("./routes/tarefa");
+const auth = require("./routes/auth");
+const usuarios = require("./routes/usuario");
+const tarefas = require("./routes/tarefa");
 
 app.use(express.json());
 
 // Rotas
-app.use("/api/auth", authRoute);
-app.use("/api/usuarios", usuariosRoute);
-app.use("/api/tarefas", tarefaRoute);
+app.use("/api/auth", auth);
+app.use("/api/usuarios", usuarios);
+app.use("/api/tarefas", tarefas);
 
 module.exports = app;
